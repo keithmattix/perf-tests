8f9ebf24374399fb62271d1a8f6695b823f5cbb4
Keith Mattix II <keithmattix@microsoft.com>
2024-08-26 23:35:05 +0000

    Add Istio ambient L7 perf tests
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

---
+++ istio-ambient/README.md
@@ -0,0 +1,3 @@
+# Istio Ambient Perf Test docs
+
+Download the latest istioctl using the instructions from [here](https://istio.io/latest/docs/ops/diagnostic-tools/istioctl/#install-hahahugoshortcode708s2hbhb)
--- istio-ambient/ambient-l7.sh
+++ istio-ambient/ambient-l7.sh
@@ -9,7 +9,6 @@ helm repo add cilium https://helm.cilium.io/
 
 helm install cilium -n kube-system cilium/cilium --version 1.16.1 \
 --set debug.verbose=datapath \
---set l7Proxy=true \
 --set kubeProxyReplacement=true \
 --set k8sServiceHost="$(kubectl get endpoints kubernetes -n default -ojson | jq '.subsets[0].addresses[0].ip' -r)" \
 --set k8sServicePort=6443 \
@@ -17,37 +16,54 @@ helm install cilium -n kube-system cilium/cilium --version 1.16.1 \
 
 kubectl wait --for=condition=ready pod --selector=k8s-app=cilium --timeout=120s -n kube-system
 
+# Add gateway API CRDs
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || { kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml; }
+
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/high-availability.yaml
 
 kubectl patch -n kube-system deployment metrics-server --type=json -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
 
 kubectl wait --for=condition=ready pod --selector=k8s-app=kube-dns --timeout=120s -n kube-system
 
-# Apply an L7 CNP to allow all HTTP requests to port 8080
-kubectl apply -f - <<EOF
-apiVersion: "cilium.io/v2"
-kind: CiliumNetworkPolicy
-metadata:
-  name: "l7-rule"
-spec:
-  endpointSelector:
-    matchLabels:
-      app: fortio
-  ingress:
-  - toPorts:
-    - ports:
-      - port: '8080'
-        protocol: TCP
-      rules:
-        http:
-        - method: ~
-EOF
+# Install Istio Ambient
+istioctl install --set profile=ambient --set tag=1.23.0 --set hub=docker.io/istio --revision=1-23-0 --set values.defaultRevision=1-23-0 -y
+
+kubectl wait --for=condition=ready pod --selector=app=istiod --timeout=120s -n istio-system
+kubectl wait --for=condition=ready pod --selector=app=ztunnel --timeout=120s -n istio-system
 
 # Fortio Server
 kubectl apply -f ../fortio-server.yaml
 
 kubectl wait --for=condition=ready pod --selector=app=fortio --timeout=120s
 
+# Add the default namespace to the mesh (including the already running server)
+kubectl label namespace default istio.io/dataplane-mode=ambient
+
+# Create a waypoint for the namespace
+istioctl waypoint apply --enroll-namespace --wait 120s
+
+WAYPOINT_POD_NAME=$(kubectl get po -listio.io/gateway-name=waypoint -o jsonpath="{.items[*]['metadata.name']}")
+
+istioctl pc log "$WAYPOINT_POD_NAME" --level debug
+
+# Now create an L7 policy to allow all HTTP requests to port 8080
+kubectl apply -f - <<EOF
+apiVersion: security.istio.io/v1
+kind: AuthorizationPolicy
+metadata:
+  name: l7-rule
+  namespace: default
+spec:
+  targetRefs:
+  - kind: Service
+    group: ""
+    name: fortio
+  action: ALLOW
+  rules:
+  - to:
+    - operation:
+        ports: ["8080"]
+EOF
 
 # Fortio Client
 kubectl apply -f ../fortio-client.yaml
@@ -58,10 +74,8 @@ kubectl wait --for=condition=ready pod --selector=app=fortio-client --timeout=12
 # Copy that JSON and paste it into a file called "cilium-l7.json"
 kubectl logs -f "$(kubectl get pods -l app=fortio-client -ojson | jq -r '.items[0].metadata.name')"
 
-# Optionally, in another tab, watch the resource usage of the node-level Envoy:
-NODE_NAME=$(kubectl get pod -lapp=fortio -o jsonpath="{.items[*]['spec.nodeName']}")
-ENVOY_POD_NAME=$(kubectl get po -lk8s-app=cilium-envoy --field-selector spec.nodeName="$NODE_NAME" -n kube-system -o jsonpath="{.items[*]['metadata.name']}")
-watch kubectl top pod "$ENVOY_POD_NAME" -n kube-system --containers
+# Optionally, in another tab, watch the resource usage of the waypoint Envoy:
+watch kubectl top pod "$WAYPOINT_POD_NAME" --containers
 
 # Now run the following command (locally) to spin up the fortio UI and view the report
 # I develop in a devcontainer, so I download the json files to my local downloads folder
